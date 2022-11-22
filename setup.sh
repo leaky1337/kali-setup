a68ca8717cae558edb255db7227b82fa666b2feb
leaky1337 <88828885+leaky1337@users.noreply.github.com>
2022-11-22 09:41:40 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install terminator
+sudo apt install xclip
+
+cp vimrc ~/.vimrc
+mkdir -p ~/.config/terminator/
+cp terminator.conf ~/.config/terminator/config
+cp tmux.conf ~/.tmux.conf
+cp nanorc ~/.nanorc
---
+++ vimrc
@@ -0,0 +1,8 @@
+"   put file to ~/.vimrc
+
+set list listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<
+set nu
+filetype plugin indent on
+set tabstop=4
+set shiftwidth=4
+set expandtab
